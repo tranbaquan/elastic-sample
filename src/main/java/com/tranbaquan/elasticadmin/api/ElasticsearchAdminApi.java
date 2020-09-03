3259467e313bc51aed3d1a63307705aa570e9f01
Quan <tranbaquan.tbq@gmail.com>
2020-09-03 10:03:44 +0700

    Add api for testing

--- src/main/java/com/tranbaquan/elasticadmin/api/ElasticsearchAdminApi.java
+++ src/main/java/com/tranbaquan/elasticadmin/api/ElasticsearchAdminApi.java
@@ -8,21 +8,28 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.GetFieldMappingsRequest;
 import org.elasticsearch.client.indices.GetFieldMappingsResponse;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilder;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval;
+import org.elasticsearch.search.aggregations.bucket.histogram.Histogram;
+import org.elasticsearch.search.aggregations.bucket.histogram.ParsedDateHistogram;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/admin")
 public class ElasticsearchAdminApi {
 
     private RestHighLevelClient elasticsearchClient;
@@ -35,7 +42,7 @@ public class ElasticsearchAdminApi {
         this.elasticsearchClient = elasticsearchClient;
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "${crossOrigin.origin}")
     @GetMapping(value = "/fields")
     public Mono<Set<String>> getAllFields() {
         GetFieldMappingsRequest request = new GetFieldMappingsRequest().indices(indexName).fields("*");
@@ -43,7 +50,8 @@ public class ElasticsearchAdminApi {
         return Mono.create(sink -> elasticsearchClient.indices().getFieldMappingAsync(request, RequestOptions.DEFAULT, new ActionListener<GetFieldMappingsResponse>() {
             @Override
             public void onResponse(GetFieldMappingsResponse response) {
-                sink.success(response.mappings().get(indexName).keySet());
+                sink.success(response.mappings().get(indexName).keySet().stream()
+                        .filter(field -> !field.startsWith("_")).collect(Collectors.toSet()));
             }
 
             @Override
@@ -53,7 +61,7 @@ public class ElasticsearchAdminApi {
         }));
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "${crossOrigin.origin}")
     @GetMapping("/chart-data")
     public Mono<ChartData> getBarCharts(@RequestParam String fieldName) {
         AggregationBuilder aggregations = AggregationBuilders.terms("agg").field(fieldName);
@@ -65,10 +73,41 @@ public class ElasticsearchAdminApi {
             public void onResponse(SearchResponse searchResponse) {
                 Terms agg = searchResponse.getAggregations().get("agg");
                 ChartData chart = new ChartData();
-                for (Terms.Bucket bucket: agg.getBuckets()) {
+                for (Terms.Bucket bucket : agg.getBuckets()) {
                     chart.getLabels().add(bucket.getKey().toString());
                     chart.getData().add(bucket.getDocCount());
                 }
+                sink.success(chart);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                sink.error(e);
+            }
+        }));
+    }
+
+    @CrossOrigin(origins = "${crossOrigin.origin}")
+    @GetMapping("chart-data/order")
+    public Mono<ChartData> getOrdersLastDay() {
+        AggregationBuilder aggregations = AggregationBuilders.dateHistogram("orders_last_day").field("order_date")
+                .calendarInterval(DateHistogramInterval.HOUR);
+        SearchSourceBuilder builder = new SearchSourceBuilder().size(0).aggregation(aggregations)
+                .query(QueryBuilders.rangeQuery("order_date").gte("now-1d/d").lte("now/d"));
+        SearchRequest request = new SearchRequest().indices(indexName).source(builder);
+
+        return Mono.create(sink -> elasticsearchClient.searchAsync(request, RequestOptions.DEFAULT, new ActionListener<SearchResponse>() {
+            @Override
+            public void onResponse(SearchResponse searchResponse) {
+                ParsedDateHistogram agg = searchResponse.getAggregations().get("orders_last_day");
+                ChartData chart = new ChartData();
+                String label;
+                for (Histogram.Bucket bucket : agg.getBuckets()) {
+                    label = bucket.getKeyAsString().substring(0, bucket.getKeyAsString().length() - 5);
+                    chart.getLabels().add(label);
+                    chart.getData().add(bucket.getDocCount());
+                }
+
                 sink.success(chart);
             }
 
